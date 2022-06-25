3df56a91e1181e771434dc425da8c1bae49f013e
Paul Rodwell <paul.rodwell@btinternet.com>
2022-06-25 10:13:50 +0100

    Move image plugin client from wiki client
    See wiki-client@74c63a762a05dee6e290b4546c9cbef06211916c

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,9 @@
 module.exports = function (grunt) {
 
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-git-authors');
 
   grunt.initConfig({
@@ -11,8 +15,42 @@ module.exports = function (grunt) {
       ]
     },
 
+    // tidy-up before we start the build
+    clean: ['client/image.js', 'client/image.js.map', 'test/test.js', 'test/test.js.map'],
+
+    browserify: {
+      plugin: {
+        src: ['client/image.coffee'],
+        dest: 'client/image.js',
+        options: {
+          transform: [[ 'coffeeify', { transpile: { presets: ['@babel/preset-env'] } } ]],
+          browserifyOptions: {
+            extentions: ".coffee"
+          }
+        }
+      }
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          require: 'coffeescript/register'
+        },
+        src: ['test/test.coffee']
+      }
+    },
+
+
+    watch: {
+      all: {
+        files: ['client/*.coffee', 'test/*.coffee'],
+        tasks: ['build']
+      }
+    }
   });
 
-  grunt.registerTask('default', ['update-authors']);
+  grunt.registerTask('build', ['clean', 'mochaTest', 'browserify']);
+  grunt.registerTask('default', ['build']);
 
 };
